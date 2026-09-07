0a44c9730736a80a204e05717ba9a7ef2684fe8c
alejaabruaamor-blip <alejaabruaamor@gmail.com>
2026-09-07 18:23:54 -0300

    chore: endpoint de diagnostico

--- api/ping.js
+++ api/ping.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).json({ ok: true, node: process.version });
+  res.status(200).json({ ok: true, node: process.version, haspub: !!process.env.FREEPAY_PUBLIC_KEY, hassec: !!process.env.FREEPAY_SECRET_KEY });
 }
